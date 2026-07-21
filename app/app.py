22236455c1e0ccb5dafe90f829f837093bc6c5ea
amanmulani09 <mulaniaman0504@gmail.com>
2026-07-21 21:26:47 +0530

    feat(app) : define imports and logging

--- app/app.py
+++ app/app.py
@@ -5,3 +5,31 @@ import json
 import base64
 import os 
 from typing import Dict,Any,List
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from datetime import datetime
+import tempfile
+from dotenv import load_dotenv, find_dotenv
+import openai
+import logging
+
+
+# setup logging 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI(title="Chitra.ai AI video analysis app", version="0.0.1")
+
+templates = Jinja2Templates(directory=".")
+
+# Configuration
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
+FROM_EMAIL = os.getenv("FROM_EMAIL")
+
+
+# Initialize services
+openai_client = openai.AsyncOpenAI(api_key=OPENAI_API_KEY)
